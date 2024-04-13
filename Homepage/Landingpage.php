ccb896e36fd269452cee5b3a21f73d6acb33342f
maina737 <nderituc38@gmail.com>
2024-04-13 10:00:24 +0300

    new file:   Homepage/Landingpage.php 	new file:   Homepage/Landingpagenav.php 	modified:   index.css

---
+++ Homepage/Landingpage.php
@@ -0,0 +1,30 @@
+
+  <?php include('../Homepage/Landingpagenav.php') ?>;
+
+
+
+  <div class="content_box">
+
+    <div class="container flex">
+      <div class="col">
+        <p class="title">
+          Find your Nearest
+          Electric Vehicle
+          Charging Station
+        </p>
+        <p class="description">
+          Charging Station Locator is the Ultimate Web
+          Application and USSD Service for Electric Vehicle
+          Drivers who Need to Find a charging Station Near their Location.Our Locator System ensures that you Will Never be Stuck With an Empty Battery Again.
+        </p>
+        <div class="custom_btn" style="margin-top:2rem;">
+          <a href="../User/./UserLogin.php" class="hero-btn">GET STARTED</a>
+        </div>
+      </div>
+      <div class="col">
+        <!-- <img src="../images/scr7.png" alt="" class="banner__displayer"> -->
+      </div>
+    </div>
+  </div>
+
+</div>
---
+++ Homepage/Landingpagenav.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../index.css">
+  <link rel="stylesheet" href="../Homepage/homepage.css">
+  <title>CHARGING STATION</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+<div class="nav" id="nav">
+      <div class="container flex">
+        <div class="logo">
+          <a href="#" class="logos">
+            charging station locator
+          </a>
+        </div>
+        
+      </div>
+    </div>
+</body>
+</html>
+
+
